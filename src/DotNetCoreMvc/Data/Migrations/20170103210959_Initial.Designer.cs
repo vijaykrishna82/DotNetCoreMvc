8cf33228f53aa53f92b026526a9b76eaf09abcb3
vijay.vepakomma@philips.com <vijay.vepakomma@philips.com>
2017-01-03 16:13:19 -0500

    Data migrations, movies

--- src/DotNetCoreMvc/Data/Migrations/20170103210959_Initial.Designer.cs
+++ src/DotNetCoreMvc/Data/Migrations/20170103210959_Initial.Designer.cs
@@ -8,9 +8,10 @@ using DotNetCoreMvc.Data;
 namespace DotNetCoreMvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170103210959_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
